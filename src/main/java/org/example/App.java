07ea04ac7f71a2c4581c83544bbe4e978767d471
howmehow <howmehow@gmail.com>
2021-06-25 11:04:46 +0200

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        var actualNames = new String[]{
+                "Johnson", "Wilson", "Wilkinson", "Abraham", "Dagobert"};
+        Sort.sort(actualNames);
+        for (final String name : actualNames) {
+            System.out.println(name);
+        }
+    }
+}
