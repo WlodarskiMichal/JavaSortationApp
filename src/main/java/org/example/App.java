f788e31192c8dcdddcc27f959477ae4089383ce0
howmehow <howmehow@gmail.com>
2021-06-26 23:07:14 +0200

    Changing function to work with Java collections method of sorting everything.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,18 +1,24 @@
 package org.example;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        var actualNames = new String[]{
-                "Johnson", "Wilson", "Wilkinson", "Abraham", "Dagobert"};
-        Sort.sort(actualNames);
-        for (final String name : actualNames) {
-            System.out.println(name);
+public class App {
+    public static void main(String[] args) {
+
+            var actualNames = new ArrayList(Arrays.asList(
+                    "Johnson", "Wilson", "Wilkinson", "Abraham", "Dagobert"));
+            Collections.sort(actualNames);
+            Assert.assertEquals(new ArrayList<>(Arrays.asList(
+                    "Abraham", "Dagobert", "Johnson", "Wilkinson", "Wilson")), actualNames);
         }
-    }
+    
 }
